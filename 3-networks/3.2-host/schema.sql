439b07c4921c5dbe7f83f1df3890c68557f975f5
alexfdealmeida <alexferreiradealmeida@gmail.com>
2022-12-16 14:25:26 -0300

    Branch: main; Atualizado nome do banco.

--- 3-networks/3.2-host/schema.sql
+++ 3-networks/3.2-host/schema.sql
@@ -1,7 +1,7 @@
-CREATE python_flask_docker;
-USE python_flask_docker;
+CREATE python_flask_host;
+USE python_flask_host;
 
-CREATE TABLE `python_flask_docker`.`users` (
+CREATE TABLE `python_flask_host`.`users` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(255),
 	PRIMARY KEY (ID);
