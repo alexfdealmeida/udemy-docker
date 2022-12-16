ae538ec31fc8b97044d0650ee601bf1281bb0627
alexfdealmeida <alexferreiradealmeida@gmail.com>
2022-12-16 14:24:08 -0300

    Branch: main; Adicionado SQL para criação do banco e da tabela.

---
+++ 3-networks/3.2-host/schema.sql
@@ -0,0 +1,8 @@
+CREATE python_flask_docker;
+USE python_flask_docker;
+
+CREATE TABLE `python_flask_docker`.`users` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255),
+	PRIMARY KEY (ID);
+)
